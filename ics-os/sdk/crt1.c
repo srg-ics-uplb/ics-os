54da446545f8692439721897b1d339efb91a9157
Matthew Marcos <matthewmarcos94@gmail.com>
2016-04-28 14:06:57 +0800

    Change conventions in some files

--- ics-os/sdk/crt1.c
+++ ics-os/sdk/crt1.c
@@ -1,23 +1,22 @@
 #include "dexsdk.h"
 
-int _start() 
-{
-   char *s;
-   int c=0;
-   char *p[100];
-   char params[500];
-   
-   getparameters(params);
-   
-   s=strtok(params," ");
-   
-   do {
-        p[c]=s;
+int _start(){
+    char *s;
+    int c = 0;
+    char *p[100];
+    char params[500];
+
+    getparameters(params);
+
+    s = strtok(params, " ");
+
+    do {
+        p[c] = s;
         c++;
-        s=strtok(0," ");
-   } while (s!=0);
-   
-   main(c,p);
-   exit(0);
-   return 0;
+        s = strtok(0, " ");
+    } while(s != 0);
+
+    main(c, p);
+    exit(0);
+    return 0;
 }
